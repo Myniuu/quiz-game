2f77df46c1dda964d596a5edd6404d0af7443ce7
Myniuu <michalszczygiel96@gmail.com>
2024-04-14 11:20:40 +0200

    Initial setup for quiz game

---
+++ quiz-game/src/App.jsx
@@ -0,0 +1,10 @@
+import styles from "./App.module.css";
+import { Header } from "./components/Header/Header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
---
+++ quiz-game/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div>
+      <h1>Quiz Game</h1>
+    </div>
+  );
+}
